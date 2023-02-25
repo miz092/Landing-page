59da2289b6d495afae3cf6b011bcf98ae900cf2b
miz092 <mihalyfi.zoltan@gmail.com>
2023-02-25 10:32:16 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <hi>Task tiger</hi>
+    </div>
+  );
+}
+
+export default App;
