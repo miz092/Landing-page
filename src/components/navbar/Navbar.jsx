c34e7aa3127f48b98a5d45e8bb9451079f0f5e12
miz092 <mihalyfi.zoltan@gmail.com>
2023-02-25 12:57:50 +0100

    implement responsiveness for the the navbar for tablets and phones

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,29 @@ import React from "react";
 import "./Navbar.css";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/Logo.png";
-
+import { useState } from "react";
 function Navbar() {
+  const Menu = () => (
+    <>
+      {" "}
+      <p>
+        <a href="#home">Home</a>
+      </p>
+      <p>
+        <a href="#whatis">About us</a>
+      </p>
+      <p>
+        <a href="#future">Future</a>
+      </p>
+      <p>
+        <a href="#features">Features</a>
+      </p>
+      <p>
+        <a href="#blog">Updates</a>
+      </p>
+    </>
+  );
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="taskTiger__navbar">
       <div className="taskTiger__navbar-links">
@@ -11,28 +32,40 @@ function Navbar() {
           <img src={logo} alt="logo" />
         </div>
         <div className="taskTiger__navbar-links_container">
-          <p>
-            <a href="#home">Home</a>
-          </p>
-          <p>
-            <a href="#whatis">About us</a>
-          </p>
-          <p>
-            <a href="#future">Future</a>
-          </p>
-          <p>
-            <a href="#features">Features</a>
-          </p>
-          <p>
-            <a href="#blog">Updates</a>
-          </p>
+          <Menu />
         </div>
       </div>
       <div className="taskTiger__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign up</button>
       </div>
-      <div></div>
+      <div className="taskTiger__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="taskTiger__navbar-menu_container scale-up-center">
+            <div className="taskTiger__navbar-menu_container-links">
+              {" "}
+              <Menu />
+              <div className="taskTiger__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
