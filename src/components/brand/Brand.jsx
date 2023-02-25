eef78410925f2e9042fa99247977c0f4cd84e055
miz092 <mihalyfi.zoltan@gmail.com>
2023-02-25 15:11:23 +0100

    will implement partner brands section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 import "./Brand.css";
+import {
+  Ikea,
+  Foodpanda,
+  Doordash,
+  Obi,
+  Taskrabbit,
+  Ubereats,
+} from "./import.js";
+
 function Brand() {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={Ikea} />
+      </div>
+      <div>
+        <img src={Foodpanda} />
+      </div>
+      <div>
+        <img src={Doordash} />
+      </div>
+      <div>
+        <img src={Obi} />
+      </div>
+      <div>
+        <img src={Taskrabbit} />
+      </div>
+      <div>
+        <img src={Ubereats} />
+      </div>
+    </div>
+  );
 }
 
 export default Brand;
