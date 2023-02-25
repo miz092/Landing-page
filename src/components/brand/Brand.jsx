f528b934954ae12f24d6b917a0eb1915ab08a217
miz092 <mihalyfi.zoltan@gmail.com>
2023-02-25 16:53:41 +0100

    will implement brands with correct css

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import "./Brand.css";
+import { foodpanda, ikea, obi, doordash, taskrabbit } from "./imports";
+
 function Brand() {
-  return <div>Brand</div>;
+  <div className="taskTiger__brand section__padding">
+    <div>
+      <img src={foodpanda} />
+    </div>
+    <div>
+      <img src={ikea} />
+    </div>
+    <div>
+      <img src={obi} />
+    </div>
+    <div>
+      <img src={doordash} />
+    </div>
+    <div>
+      <img src={taskrabbit} />
+    </div>
+  </div>;
 }
 
 export default Brand;
---
+++ src/components/brand/imports.js
@@ -0,0 +1,7 @@
+import ikea from "../../assets/ikea.png";
+import doordash from "../../assets/doordash.png";
+import taskrabbit from "../../assets/taskrabbit.png";
+import foodpanda from "../../assets/foodpanda.png";
+import obi from "../../assets/obi.png";
+
+export { ikea, doordash, taskrabbit, foodpanda, obi };
