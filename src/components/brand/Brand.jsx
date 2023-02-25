ce9e5aadb77a26c283ed8bed36d7e2b46f123b27
miz092 <mihalyfi.zoltan@gmail.com>
2023-02-25 16:34:36 +0100

    Revert "will implement partner brands section"
    This reverts commit eef78410925f2e9042fa99247977c0f4cd84e055.

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,37 +1,7 @@
 import React from "react";
 import "./Brand.css";
-import {
-  Ikea,
-  Foodpanda,
-  Doordash,
-  Obi,
-  Taskrabbit,
-  Ubereats,
-} from "./import.js";
-
 function Brand() {
-  return (
-    <div className="gpt3__brand section__padding">
-      <div>
-        <img src={Ikea} />
-      </div>
-      <div>
-        <img src={Foodpanda} />
-      </div>
-      <div>
-        <img src={Doordash} />
-      </div>
-      <div>
-        <img src={Obi} />
-      </div>
-      <div>
-        <img src={Taskrabbit} />
-      </div>
-      <div>
-        <img src={Ubereats} />
-      </div>
-    </div>
-  );
+  return <div>Brand</div>;
 }
 
 export default Brand;
--- src/components/brand/import.js
+++
@@ -1,8 +0,0 @@
-import Ikea from "../../assets/ikea.png";
-import Foodpanda from "../../assets/foodpanda.png";
-import Doordash from "../../assets/doordash.png";
-import Obi from "../../assets/obi.png";
-import Taskrabbit from "../../assets/taskrabbit.png";
-import Ubereats from "../../assets/ubereats.png";
-
-export { Ikea, Foodpanda, Doordash, Obi, Taskrabbit, Ubereats };
